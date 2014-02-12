752b9a10ba1f5c6b60c7a7f39a072372bf5f2d71
wils0n <wiljm0.0@gmail.com>
2014-02-11 20:36:16 -0500

    add draw maps

---
+++ GosuTutorial/draw/maps/game_windows.rb
@@ -0,0 +1,33 @@
+require 'hasu'
+
+Hasu.load 'map.rb'
+
+class GameWindow < Hasu::Window
+	WIDTH = 1000
+	HEIGHT = 500
+
+	def initialize
+		super(WIDTH, HEIGHT, false)
+		@sky = Gosu::Image.new(self, "media/bg_shroom.png", true)
+	end
+
+	def reset
+		@map = Map.new(self)
+
+	end
+
+	def draw
+		@sky.draw(0, 0, 0)
+	    translate(-@camera_x, -@camera_y) do
+	      @map.draw
+	    end
+
+	end
+
+
+	def update
+		
+	end
+end
+
+GameWindow.run
